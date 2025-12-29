ff49054c9499feedcda7ba657340423f2ab4db8f
Mariojuniorchimene <36141@esjaloures.org>
2025-12-29 20:38:32 +0000

    criando o extends das classes para herdarem os atributos da classe produto

--- Bebida.java
+++ Bebida.java
@@ -1,22 +1,7 @@
-
-/**
- * Escreva uma descrição da classe Bebida aqui.
- * 
- * @author (seu nome) 
- * @version (um número da versão ou uma data)
- */
 public class Bebida extends Produto
 {
-    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
-    private static final String TIPO = "Bebida";
-    private double qt_litros;
-    /**
-     * Construtor para objetos da classe Bebida
-     */
-    public Bebida(String nome, double preco, int qt_disp, String img, double qt_litros)
+    public Bebida(String nome, double preco, int quantidade, String imagem)
     {
-        super(nome,preco,qt_disp,img);
-        
-        this.qt_litros = qt_litros;
+        super(nome, preco, quantidade, imagem);
     }
 }
--- Snack.java
+++ Snack.java
@@ -1,33 +1,7 @@
-
-/**
- * Escreva uma descrição da classe Snack aqui.
- * 
- * @author (seu nome) 
- * @version (um número da versão ou uma data)
- */
-public class Snack
+public class Snack extends Produto
 {
-    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
-    private int x;
-
-    /**
-     * Construtor para objetos da classe Snack
-     */
-    public Snack()
+    public Snack(String nome, double preco, int quantidade, String imagem)
     {
-        // inicializa variáveis de instância
-        x = 0;
-    }
-
-    /**
-     * Um exemplo de um método - substitua este comentário pelo seu próprio
-     * 
-     * @param  y   um exemplo de um parâmetro de método
-     * @return     a soma de x e y 
-     */
-    public int sampleMethod(int y)
-    {
-        // escreva seu código aqui
-        return x + y;
+        super(nome, preco, quantidade, imagem);
     }
 }
