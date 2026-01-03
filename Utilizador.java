8f7f1e3d069b67c00c1f3113089341bd03fe968d
Gerry <madeiramateus@gmai.com>
2026-01-03 16:12:17 +0000

    criaçao dos metodos carregar produto e criar saldo.

--- Utilizador.java
+++ Utilizador.java
@@ -22,4 +22,41 @@ public class Utilizador
         
         this.listaDeProdutos = new ArrayList<Produto>();
     }
+    
+    public double getSaldo() {
+        return saldo;
+        }
+        
+       public void carregarSaldo(double valor) {
+        if (valor > 0) {
+            saldo += valor;
+            System.out.println("Carregamento efetuado com sucesso");
+        }
+    }
+    
+     public void comprarProduto(Produto produto) {
+
+        // a) verificar saldo suficiente
+        if (saldo >= produto.getPreco()) {
+
+            // b) verificar se o produto está disponível
+            if (produto.getQuantidade() > 0) {
+
+                // c) diminuir saldo do utilizador
+                saldo -= produto.getPreco();
+
+                // d) diminuir quantidade do produto
+                produto.comprar();
+
+                // e) mensagem de sucesso
+                System.out.println("Produto comprado com sucesso");
+
+            } else {
+                System.out.println("Produto esgotado");
+            }
+
+        } else {
+            System.out.println("Saldo insuficiente");
+        }
+    }
 }
